69c035d607703361378642863708537ca40a787f
GPortas <guillermoportasgonzalez@gmail.com>
2019-03-31 13:31:10 +0200

    Allowing instance outbound traffic to everywhere

--- terraform/002_ec2.tf
+++ terraform/002_ec2.tf
@@ -4,11 +4,20 @@ resource "aws_security_group" "ec2_instance_sg" {
 }
 
 
-resource "aws_security_group_rule" "ec2_instance_sg_rule" {
+resource "aws_security_group_rule" "ec2_instance_sg_rule_ingress" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.ec2_instance_sg.id}"
+}
+
+resource "aws_security_group_rule" "ec2_instance_sg_rule_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "${aws_security_group.ec2_instance_sg.id}"
 }
